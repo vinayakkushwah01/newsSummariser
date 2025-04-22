d87c3847c25aebfb1a01cb2c36565ebcded1ca4e
vinayak kushwah <kushwahvinayak1@gmail.com>
2025-04-22 18:45:37 +0530

    Added Api for Profile view And For update password

--- src/main/java/com/newsSummeriser/Config/WebConfig.java
+++ src/main/java/com/newsSummeriser/Config/WebConfig.java
@@ -2,6 +2,7 @@ package com.newsSummeriser.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,10 @@ public class WebConfig {
             }
         };
         
+    }
+
+      @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
